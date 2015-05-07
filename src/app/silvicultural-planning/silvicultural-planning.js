6aa6c9f6a8cd38265769c512dfbf2c8193af0759
Julien Künzi <julien.kuenzi@bfh.ch>
2015-05-07 14:29:20 +0200

    silvicultural planning

--- src/app/silvicultural-planning/silvicultural-planning.js
+++ src/app/silvicultural-planning/silvicultural-planning.js
@@ -16,18 +16,34 @@
 'use strict';
 
 (function() {
-	angular.module('focusApp.silviculturalPlanning', [ 'focusApp.dataService' ])
+	angular.module('focusApp.silviculturalPlanning', [ 'focusApp.dataService', 'focusApp.navigationService' ])
 
-	.controller('SiviculturalPlanningController',
-			[ 'DataService', function(DataService) {
-
+	.controller('SilviculturalPlanningController',
+			[ '$location', 'DataService', 'NavigationService', function($location, DataService, NavigationService) {
 				var _self = this;
 
 				/**
+				 * Reference to DataService 
+				 */
+				_self.dataSvc = DataService;
+
+				/**
+				 * Get the id of the currently accessed machine
+				 */
+				var parts = $location.path().split(/\//);
+				_self.currentMachineId = parts[2] || 0;
+				
+				/**
 				 * Reference to the current data sample being rendered
 				 */
-				_self.data = DataService.data;
-
+				_self.data = DataService.data.machine[_self.currentMachineId];
+	
+				/**
+				 * Set the title of the page
+				 */
+				NavigationService.currentTitle = 'Silvicultural operation planning - ' + _self.data.name;
+				console.log(_self.data);
+				
 			} ]);
 
 }());
